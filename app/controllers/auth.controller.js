df5b0e9d96c8d13760eafdb951df0fede9caa144
Sankeerth Manchi <sankeerth.manchi@eagles.oc.edu>
2024-05-29 21:25:19 -0400

    Login completed

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -31,6 +31,7 @@ exports.login = async (req, res) => {
         lastName: user.lastName,
         id: user.id,
         token: token,
+        type: user.type
       };
       res.send(userInfo);
     });
